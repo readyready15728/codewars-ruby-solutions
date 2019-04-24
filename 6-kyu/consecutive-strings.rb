7edfc01945198f8b748f15ba241ace350944977c
Lynn Bradshaw <readyready15728@gmail.com>
2019-04-23 22:12:42 -0400

    Adding solution for "Consecutive strings"

---
+++ 6-kyu/consecutive-strings.rb
@@ -0,0 +1,14 @@
+def longest_consec(strarr, n)
+    if strarr.length == 0 or n > strarr.length or n <= 0
+      return ''
+    end
+    
+    n_grams = []
+    
+    for i in (0...strarr.length - (n - 1))
+      n_grams.append(strarr[i...i +  n].join(""))
+    end
+    
+    n_gram_lengths = n_grams.collect { |s| s.length }
+    n_grams[n_gram_lengths.index(n_gram_lengths.max)]
+end
